0277ddc7f0d45c9bd58efe8a0ac9f4695b3b82fe
filland99 <55227021+filland99@users.noreply.github.com>
2022-05-31 10:54:15 +0200

    Update power-up.ps1

--- power-up.ps1
+++ power-up.ps1
@@ -6,14 +6,14 @@ $password = ConvertTo-SecureString 'fiVvH_7HCCYZvXed' -AsPlainText -Force
 new-localuser -Name telepo -Password $password
 add-localgroupmember -Group administrators -Member telepo
  
-Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
-New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
-Get-Service sshd | Set-Service -StartupType Automatic
-Start-Service sshd
+#Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+#New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
+#Get-Service sshd | Set-Service -StartupType Automatic
+#Start-Service sshd
  
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install cygwin cyg-get -y
-cyg-get openssh python38 python38-pip python38-devel libssl-devel libffi-devel gcc-g++
+#choco install cygwin cyg-get -y
+#cyg-get openssh python38 python38-pip python38-devel libssl-devel libffi-devel gcc-g++
 choco install 7zip -y
 # This script installs IIS and the features required to
 # run West Wind Web Connection.
